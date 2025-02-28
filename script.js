71bd9256fb5ba9b80c12cd8cb96170f1e80d55b9
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-28 09:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //your JS code here. If required.
 const Button=document.querySelector('#submit');
-Button.addEventHandler('click',()=>{
-	const
+Button.addEventListener('click',()=>{
+	const FirstName=document.querySelector('#firstName');
+	const LastName=document.querySelector('#lastName');
+	const Phone=document.querySelector('#phone');
+	const Email=document.querySelector('#email');
+
+	alert(`First Name: ${FirstName.value} Last Name: ${LastName.value} Phone Number: ${Phone.value} Email ID: ${Email.value}`)
 })
